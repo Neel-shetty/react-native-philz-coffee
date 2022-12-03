c4147aae6edf30befd29a1166bbe3341f2f94399
Neel <neelnarayanshetty@gmail.com>
2022-12-04 01:20:06 +0530

    scaling works

--- components/PhilzCoffee/Products.tsx
+++ components/PhilzCoffee/Products.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { View, StyleSheet, Image, Dimensions } from "react-native";
-import Animated from "react-native-reanimated";
+import Animated, {
+  interpolate,
+  interpolateColor,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 import { products } from "./Model";
 
@@ -11,6 +15,7 @@ const styles = StyleSheet.create({
     ...StyleSheet.absoluteFillObject,
     justifyContent: "center",
     alignItems: "center",
+    top:20
   },
 });
 
@@ -22,13 +27,33 @@ const Products = ({ x }: ProductsProps) => {
   return (
     <View style={styles.container} pointerEvents="none">
       {products.map((product, index) => {
+        const style = useAnimatedStyle(() => {
+          const inputRange = [
+            width * (index - 1),
+            width * index,
+            width * (index + 1),
+          ];
+          const translateX = interpolate(x.value, inputRange, [
+            width / 2,
+            0,
+            -width / 2,
+          ]);
+          const scale = interpolate(x.value, inputRange, [
+            0.75,
+            1,
+            0.75,
+          ]);
+          return {
+            transform: [{ translateX }, {scale}],
+          };
+        });
         return (
-          <View key={index} style={styles.container}>
+          <Animated.View key={index} style={[styles.container, style]}>
             <Image
               source={product.picture}
               style={{ width: SIZE, height: SIZE * product.aspectRatio }}
             />
-          </View>
+          </Animated.View>
         );
       })}
     </View>
